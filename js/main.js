ef6f6dce168217b01e3dc7c230db77df0e265edb
rljusticio <j.donnell.walters@gmail.com>
2016-04-07 18:22:01 -0400

    addded custom styles

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//begin with the jQuery method of checking to ensure page elements are ready
+$(document).ready(function(){
+  //initialize the carousel with an interval of 8000 milliseconds
+  $('.carousel').carousel({
+    interval: 8000
+  });
+});
